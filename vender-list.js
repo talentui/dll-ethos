1b3d88af08952585cb384d82fcf444ef3d343ab5
lihe3 <lihe3@corp.beisencorp.com>
2017-10-13 18:03:59 +0800

    build:vender list

--- vender-list.js
+++ vender-list.js
@@ -1,7 +1,6 @@
 module.exports=[
   "@beisen/base-button",
   "@beisen/beisen-scroll-bar",
-  "@beisen/calendar",
   "@beisen/checkbox-list",
   "@beisen/common-input",
   "@beisen/common-label",
@@ -12,7 +11,6 @@ module.exports=[
   "@beisen/dropdown-button",
   "@beisen/dropdown-list",
   "@beisen/es6-promise-debounce",
-  "@beisen/grid-page-builder",
   "@beisen/input-box",
   "@beisen/loading",
   "@beisen/nade-style",
@@ -24,14 +22,11 @@ module.exports=[
   "@beisen/search",
   "@beisen/selected-component",
   "@beisen/tab-component",
-  "@beisen/talent-ui-bootstrap",
-  "@beisen/talent-ui-dll-foundation",
-  "@beisen/talent-ui-helper",
   "@beisen/text-area",
   "@beisen/text-box",
   "@beisen/time-picker",
   "@beisen/tool-tip",
   "@beisen/tree",
   "@beisen/upaas-portal-common-css",
-  "@beisen/user-selector"
+  "@beisen/user-selector",
 ]
